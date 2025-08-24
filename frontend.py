33af7b329e9c15e8bd557ee182d0579aaf955f9f
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-08-24 18:30:15 -0500

    Set the base frontend

--- frontend.py
+++ frontend.py
@@ -1,5 +1,11 @@
 # Setup Streamlit
+import streamlit as st
 
+st.set_page_config(page_title="AI Therapist for Mental Health", layout='wide')
+st.title("Placeholder name")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history =[]
 
 # User is able to ask a question
 
