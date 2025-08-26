303ead4fad0beded1865a4d52a8d2778133c1cce
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-08-26 00:47:00 -0500

    Chat input

--- frontend.py
+++ frontend.py
@@ -8,6 +8,10 @@ if "chat_history" not in st.session_state:
     st.session_state.chat_history =[]
 
 # User is able to ask a question
-
+# Chat input
+user_input = st.chat_input("What's up?")
+if user_input:
+    # Append user message
+    st.session_state.chat_history.append({"role": "user", "content": user_input})
 
 # Show response from the backend
