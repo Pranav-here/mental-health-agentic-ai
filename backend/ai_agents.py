68545aeb108516e1bfa4f98ec594297c3247b76c
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-08-28 01:40:32 -0500

    Add the mental health specialist tool

--- backend/ai_agents.py
+++ backend/ai_agents.py
@@ -1,1 +1,16 @@
+from langchain.agents import tool
+from tools import query_medgemma
+
+
+@tool
+def ask_mental_health_specialist(query: str) -> str:
+    """
+        Generate a therapuetic response using the Medgemma model
+        use this for all general user queries, mental health questions, emotional concerns,
+        or to offer empathetic, evidence-based guidance in a conversational tone.
+    """
+    return query_medgemma(query)
+
+
 # Setup AI agent and link to the backend
+
