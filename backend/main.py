2153aca54078d3dfb50349b88772397f6a7c72b1
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-08-28 00:38:18 -0500

    Setup simple fastapi backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,13 @@
 # Setup FastAPI backend
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/ask")
+async def ask():
+    # AI Agent 
+    return "This is the response"
 
 # Recieve and validate request from frontend
 
