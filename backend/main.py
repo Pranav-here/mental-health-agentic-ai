a34f8c503ca6508f585d91328e7b99b772ecf7c3
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-08-28 00:54:40 -0500

    Rearrange code

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 # Setup FastAPI backend
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -15,8 +16,9 @@ async def ask(query: Query):
     # AI Agent 
     # response = ai_agent(query)
     response = "this is from backend"
+    # Send response to the frontend
     return response
 
 
-
-# Send response to the frontend
+if __name__=="__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
